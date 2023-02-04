320385b6446fcd4967ecd2fb35218cede83c2f47
AdityaMandothia <adityamandothia@gmail.com>
2023-02-04 18:18:22 +0530

    numerical hollow pyramid final edit

--- LOOPS/numerical_hollow_pyramid.cpp
+++ LOOPS/numerical_hollow_pyramid.cpp
@@ -2,53 +2,52 @@
 using namespace std;
 
 int main()
-{   
+{
     int n;
-    cin>>n;
+    cin >> n;
 
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        for(int j=n-1-i;j>0;j--)
+        for (int j = n - 1 - i; j > 0; j--)
         {
-            cout<<" ";
+            cout << " ";
         }
 
-        int start=1;
+        int start = 1;
 
         // print full pyramid of numbers
-        for(int j=0;j<=2*i;j++)
+        for (int j = 0; j <= 2 * i; j++)
         {
-            if(i==0 ||i==n-1)
+            if (i == 0 || i == n - 1)
             {
-                if(j%2==0)
+                if (j % 2 == 0)
                 {
-                    cout<<start;
+                    cout << start;
                     start++;
                 }
-                else{
-                    cout<<" ";
+                else
+                {
+                    cout << " ";
                 }
             }
-            else{
-                if(j==0)
+            else
+            {
+                if (j == 0)
                 {
-                    cout<<1;
+                    cout << 1;
                 }
-                else if(j==2*i)
+                else if (j == 2 * i)
                 {
-                    cout<<i+1;
+                    cout << i + 1;
                 }
-                else{
-                    cout<<" ";
+                else
+                {
+                    cout << " ";
                 }
             }
-
-
         }
 
-        cout<<endl;
-
-
+        cout << endl;
     }
 
     return 0;
