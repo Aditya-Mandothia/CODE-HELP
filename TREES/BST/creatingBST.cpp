28e85bd4bc95a87e402c38ca8203c6ab5eabe85c
AdityaMandothia <adityamandothia@gmail.com>
2023-06-23 21:00:16 +0530

    BST and BT

---
+++ TREES/BST/creatingBST.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+void levelOrderTraversal(Node *root)
+{
+    queue<Node *> q;
+
+    q.push(root);
+
+    while (!q.empty())
+    {
+
+        Node *temp = q.front();
+
+        q.pop();
+
+        cout << temp->data << " ";
+
+        if (temp->left)
+        {
+            q.push(temp->left);
+        }
+
+        if (temp->right)
+        {
+            q.push(temp->right);
+        }
+    }
+}
+
+Node *insertIntoBST(Node *root, int data)
+{
+
+    if (root == NULL)
+    {
+        root = new Node(data);
+        return root;
+    }
+
+    if (root->data > data)
+    {
+        root->left = insertIntoBST(root->left, data);
+    }
+    else
+    {
+        root->left = insertIntoBST(root->left, data);
+    }
+
+    return root;
+}
+
+void takeInput(Node *&root)
+{
+    int data;
+    cin >> data;
+
+    while (data != -1)
+    {
+        root = insertIntoBST(root, data);
+        cin >> data;
+    }
+}
+
+int main()
+{
+    Node *root = NULL;
+    takeInput(root);
+    levelOrderTraversal(root);
+
+    return 0;
+}
